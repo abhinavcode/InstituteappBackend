0078930baf1a2f03eb395ffaa29ddfb934cb0b53
Karyamsetty Martin <54419980+AngryCder@users.noreply.github.com>
2020-04-22 04:58:08 +0530

    Update urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -17,5 +17,27 @@ urlpatterns = [
     url(r'^importantcontacts$', views.importantcontacts, ),
     url(r'^timetable$', views.timetable, ),
 
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)from django.conf.urls import  include, url
+from django.contrib import admin
+
+from mainapp import views
+from django.conf.urls.static import static
+
+from django.conf import settings
+
+
+
+urlpatterns = [
+    url(r'^login$', views.login, ),
+    url(r'^checkreg$', views.checkregister, ),
+    url(r'^postcomplain$', views.postcomplain, ),
+    url(r'^interested$', views.interested, ),
+    url(r'^feedandclubs$', views.feedandclubs, ),
+    url(r'^importantcontacts$', views.importantcontacts, ),
+    url(r'^timetable$', views.timetable, ),
+    url(r'^notification$', views.notification, ),
+    url(r'^pors$', views.pors, ),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
