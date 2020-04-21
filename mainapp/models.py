9675a18021d61b0f51d7c5f0cf60df503604cd16
Karyamsetty Martin <54419980+AngryCder@users.noreply.github.com>
2020-04-22 04:55:46 +0530

    Update models.py

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+import requests as req
+import json
 # Create your models here.
 class Student(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -19,7 +21,7 @@ class Student(models.Model):
 	yearOfAdmission = models.IntegerField(blank = True, default = 2018)
 	reg_time = models.DateTimeField(auto_now=True)
 	def __str__(self):
-		return self.name 
+		return self.name
 
 
 class Complain(models.Model):
@@ -30,7 +32,7 @@ class Complain(models.Model):
 	complainsubtype=models.TextField(blank=True, null=True)
 	def __str__(self):
 		if self.complainby:
-			return "complain by " + self.complainby.name 
+			return "complain by " + self.complainby.name
 		return "complain by anonymous"
 
 class CouncilandCell(models.Model):
@@ -38,15 +40,16 @@ class CouncilandCell(models.Model):
 	image = models.ImageField(null=True,blank=False)
 	def __str__(self):
 		return self.name
-	
+
 class POR(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	councilname = models.OneToOneField(CouncilandCell, on_delete = models.CASCADE)
-	postion = models.CharField(max_length=100, blank=False)
+	name = models.CharField(max_length=100, blank=False,default="")
+	email = models.CharField(max_length=100, blank=False,default="")
+	councilname = models.CharField(max_length=100, blank=False,default="")
+	postion = models.CharField(max_length=100, blank=False,default="")
 	def __str__(self):
-		return self.user.username
-	
-	
+		return self.name
+
+
 class Club(models.Model):
 	councilname = models.ForeignKey(CouncilandCell, on_delete = models.CASCADE)
 	name = models.CharField(max_length=100, blank=False)
@@ -66,12 +69,19 @@ class Notification(models.Model):
 	notification = models.CharField(max_length= 10000, verbose_name='Description')
 	notification_header = models.CharField(max_length = 100,verbose_name='Title')
 	notification_pic = models.ImageField(null=True,blank=True,verbose_name='Image')
-	datetime = models.DateTimeField()
+	datetime = models.DateTimeField(null=False, blank=False)
+	datetime.editable = True
 	location = models.CharField(max_length = 300)
 	map_location = models.CharField(max_length = 300,choices = LOCATIONS, null=True,blank=True)
 	viewedby = models.ManyToManyField(Student,editable=False,related_name='viewedby')
 	interested = models.ManyToManyField(Student,editable=False,related_name='interested')
-	
+
+	def save(self, *args, **kwargs):
+		jsondata = {"notification": {"title": self.notification_header,"body" : self.notification,"location":self.location,"year":self.datetime.year,"month":self.datetime.month,"hour":self.datetime.hour,"minutes":self.datetime.minute},"to": "/topics/"+ self.clubname.name.split()[0]}
+		firebase_messaging_req =req.post(url="https://fcm.googleapis.com/fcm/send",data=json.dumps(jsondata),headers = {"Content-Type":"application/json","Authorization":"key=AAAAIJ0yPMw:APA91bFSHkaDO3-s5c6K3U8H0LQFrU7PUz1GIMlaW5lit6dtsh46JUgvJD_cT0l79P_pJQRfeoqs57WjG9DqMdVFpglDjBl9CnZ_lINpmo7AQxol6p7U0BdpPEbh5M2PTpiCiZdx7vOP"})
+		print(firebase_messaging_req.text)
+		super(Notification, self).save(*args, **kwargs)
+
 	class Meta:
 		ordering = ['-datetime']
 
@@ -83,7 +93,7 @@ class ImpContact(models.Model):
 	email = models.CharField(max_length=100,blank=True)
 	phone = models.CharField(max_length=10,blank=True)
 	role_type = models.CharField(max_length=20,blank=True)
-	
+
 	def __str__(self):
 		return self.name
 
@@ -91,19 +101,15 @@ class TimeTable(models.Model):
 	department = models.CharField(max_length=100,blank=True)
 	year = models.IntegerField(blank = True, default = 1)
 	tableimage = models.ImageField(null=True,blank=True)
-	
+
 	def __str__(self):
 		return self.department
-	
+
 class Exam(models.Model):
 	subject_code = models.CharField(max_length=10,blank=True)
 	date_and_time = models.DateTimeField(auto_now_add=False)
 	date_and_time.editable = True
 	location = models.CharField(max_length=20,blank=True)
-	
+
 	def __str__(self):
 		return self.subject_code
-
-	
-	
-	
